1c703658836faa1e9097753c485316a80d12a6b5
Husnain Nadeem <160522514+Husnain-Nadeem@users.noreply.github.com>
2026-07-27 13:48:06 +0500

    checking if cicd by aws working

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,4 +4,4 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull husnain2812/simple-python-flask-app:latest
 # Run the Docker image as a container
-docker run -d -p 5000:5000 husnain2812/simple-python-flask-app:latest
+docker run -d -p 5000:5000 husnain2812/simple-python-flask-app:latest  
